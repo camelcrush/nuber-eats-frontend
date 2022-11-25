889e350e87f8532bf3c99aab81d702632f484a06
camelcrush <dkxm0927@naver.com>
2022-11-26 00:49:41 +0900

    Apollo, React Router Set Up

---
+++ src/apollo.ts
@@ -0,0 +1,6 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+export const client = new ApolloClient({
+  uri: "https://localhost:3000/graphql",
+  cache: new InMemoryCache(),
+});

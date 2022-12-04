b353f6f7d1cf148a981c7fc1a40b4a7a093a63a2
camelcrush <dkxm0927@naver.com>
2022-12-04 14:00:01 +0900

    Using the Token

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,3 +1,30 @@
 import React from "react";
+import { gql, useQuery } from "@apollo/client";
+import { MeQuery } from "../__generated__/graphql";
 
-export const LoggedInRouter = () => <span>Logged In</span>;
+const ME_QUERY = gql`
+  query me {
+    me {
+      id
+      email
+      role
+      verified
+    }
+  }
+`;
+
+export const LoggedInRouter = () => {
+  const { data, loading, error } = useQuery<MeQuery>(ME_QUERY);
+  if (!data || loading || error) {
+    return (
+      <div className="h-screen flex justify-center items-center">
+        <span className="font-medium text-xl tracking-wide">Loading...</span>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <span>{data?.me.email}</span>
+    </div>
+  );
+};

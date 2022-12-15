cc20bca671766710e20f551ea2a3aaa51a3721af
camelcrush <dkxm0927@naver.com>
2022-12-15 22:58:53 +0900

    CreateAccount 3: Library Mocking

--- src/pages/__test__/create-account.test.tsx
+++ src/pages/__test__/create-account.test.tsx
@@ -7,6 +7,21 @@ import { render, waitFor, RenderResult } from "../../test-utils";
 import { UserRole } from "../../__generated__/graphql";
 import { CreateAccount, CREATE_ACCOUNT_MUTATION } from "../create-account";
 
+const mockPush = jest.fn();
+
+jest.mock("react-router-dom", () => {
+  // mock이 필요한 method 외에 실제 라이브러리(모듈) 기능 추가해줘야 기존 기능들을 쓸 수가 있음
+  const realModule = jest.requireActual("react-router-dom");
+  return {
+    ...realModule,
+    useHistory: () => {
+      return {
+        push: mockPush,
+      };
+    },
+  };
+});
+
 describe("<CreateAccount />", () => {
   let mockClient: MockApolloClient;
   let renderResult: RenderResult;
@@ -67,7 +82,7 @@ describe("<CreateAccount />", () => {
         },
       },
     });
-    // window.alert 기능 spyOn하여 대신 Implementation
+    // window.alert 기능 spyOn하여 대신 Implementation 대체
     jest.spyOn(window, "alert").mockImplementation(() => null);
     mockClient.setRequestHandler(
       CREATE_ACCOUNT_MUTATION,
@@ -87,6 +102,12 @@ describe("<CreateAccount />", () => {
       });
       let mutationError = getByRole("alert");
       expect(mutationError).toHaveTextContent("mutation-error");
+      expect(mockPush).toHaveBeenCalledWith("/");
     });
+  });
+
+  // 라이브리 mock한 후에는 반드시 Clear해주기, 안 그러면 다른 테스트에 영향을 줌
+  afterAll(() => {
+    jest.clearAllMocks();
   });
 });

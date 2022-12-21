815f1d380a3a7f02b63619843f99a43f46908d68
camelcrush <dkxm0927@naver.com>
2022-12-21 22:00:43 +0900

    Owner Routes

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -9,26 +9,40 @@ import { EditProfile } from "../pages/user/edit-profile";
 import { Search } from "../pages/client/search";
 import { Category } from "../pages/client/category";
 import { Restaurant } from "../pages/client/restaurant";
+import { MyRestaurants } from "../pages/owner/my-restaurants";
 
-const ClientRoutes = [
+const clientRoutes = [
   <Route key={1} path="/" exact>
     <Restaurants />
   </Route>,
-  <Route key={2} path="/confirm">
-    <ConfirmEmail />
-  </Route>,
-  <Route key={3} path="/edit-profile">
-    <EditProfile />
-  </Route>,
-  <Route key={4} path="/search">
+  <Route key={2} path="/search">
     <Search />
   </Route>,
-  <Route key={5} path="/category/:slug">
+  <Route key={3} path="/category/:slug">
     <Category />
   </Route>,
-  <Route key={6} path="/restaurant/:id">
+  <Route key={4} path="/restaurant/:id">
     <Restaurant />
   </Route>,
+];
+
+const restaurantRoutes = [
+  <Route key={1} path="/">
+    <MyRestaurants />
+  </Route>,
+];
+
+// clientRoutes, restaurantRoutes도 아래와 같이 마찬가지로 array.map으로 코딩하려 했으나
+// 버전호환성 문제인제 라우터가 정상적으로 작동하지 않음
+const commonRoutes = [
+  {
+    path: "/confirm",
+    component: <ConfirmEmail />,
+  },
+  {
+    path: "/edit-profile",
+    component: <EditProfile />,
+  },
 ];
 
 export const LoggedInRouter = () => {
@@ -44,7 +58,13 @@ export const LoggedInRouter = () => {
     <Router>
       <Header />
       <Switch>
-        {data.me.role === "Client" && ClientRoutes}
+        {data.me.role === "Client" && clientRoutes}
+        {data.me.role === "Owner" && restaurantRoutes}
+        {commonRoutes.map((route) => (
+          <Route key={route.path} path={route.path}>
+            {route.component}
+          </Route>
+        ))}
         <Route>
           <NotFound />
         </Route>

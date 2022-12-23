040373e9f309c8cf310f61fe51817c4216284899
camelcrush <dkxm0927@naver.com>
2022-12-23 20:10:37 +0900

    Create Restaurant: File Upload

--- src/pages/owner/add-restaurnat.tsx
+++ src/pages/owner/add-restaurnat.tsx
@@ -1,8 +1,9 @@
 import { gql, useMutation } from "@apollo/client";
-import React from "react";
+import React, { useState } from "react";
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
 import { Button } from "../../components/button";
+import { FormError } from "../../components/form-error";
 import {
   CreateRestaurantMutation,
   CreateRestaurantMutationVariables,
@@ -21,29 +22,69 @@ interface IFormProps {
   name: string;
   address: string;
   categoryName: string;
+  file: FileList;
 }
 
 export const AddRestaurant = () => {
-  const [createRestaurantMutation, { data, loading }] = useMutation<
+  const onCompleted = (data: CreateRestaurantMutation) => {
+    const {
+      createRestaurant: { ok, error },
+    } = data;
+    if (ok) {
+      setUploading(false);
+    }
+  };
+  const [createRestaurantMutation, { data }] = useMutation<
     CreateRestaurantMutation,
     CreateRestaurantMutationVariables
-  >(CREATE_RESTAURANT_MUTATION);
+  >(CREATE_RESTAURANT_MUTATION, { onCompleted });
   const {
     register,
     formState: { isValid, errors },
     getValues,
     handleSubmit,
   } = useForm<IFormProps>({ mode: "onChange" });
-  const onSubmit = () => {
-    console.log(getValues());
+  const [uploading, setUploading] = useState(false);
+  const onSubmit = async () => {
+    try {
+      setUploading(true);
+      const { file, name, address, categoryName } = getValues();
+      // file의 타입은 FileList인데 배열 중 첫 번째가 파일을 가리킴
+      const actualFile = file[0];
+      // FormData : FormData 인터페이스는 form 필드와 그 값을 나타내는 일련의 key/value 쌍을 쉽게 생성할 수 있는 방법을 제공
+      const formBody = new FormData();
+      formBody.append("file", actualFile);
+      const { url: coverImg } = await (
+        await fetch("http://localhost:4000/uploads", {
+          method: "POST",
+          body: formBody,
+        })
+      ).json();
+      // S3 업로드 후 Mutation 실행
+      createRestaurantMutation({
+        variables: {
+          input: {
+            name,
+            address,
+            categoryName,
+            coverImg,
+          },
+        },
+      });
+    } catch (e) {
+      console.log(e);
+    }
   };
   return (
-    <div className="container">
+    <div className="container flex flex-col items-center mt-52">
       <Helmet>
         <title>Add Restaurant | Nuber Eats</title>
       </Helmet>
-      <h1>Add Restaurant</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <h4 className="font-semibold text-2xl mb-3">Add Restaurant</h4>
+      <form
+        className="grid max-w-screen-sm gap-3 mt-5 w-full mb-5"
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <input
           className="input"
           type="text"
@@ -62,11 +103,19 @@ export const AddRestaurant = () => {
           placeholder="Category Name"
           {...register("categoryName", { required: "Name is required" })}
         />
+        <input
+          type="file"
+          accept="image/*"
+          {...register("file", { required: true })}
+        />
         <Button
-          loading={loading}
+          loading={uploading}
           canClick={isValid}
           actionText="Create Restaurant"
         />
+        {data?.createRestaurant.error && (
+          <FormError errorMessage={data.createRestaurant.error} />
+        )}
       </form>
     </div>
   );
